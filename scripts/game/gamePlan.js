21a1c12fa0fbb6f022135f66746006f1eb318d81
Mikkel Trøst Nielsen <mtn.dtu@gmail.com>
2017-05-04 11:58:00 +0200

    added google analytics

---
+++ scripts/game/gamePlan.js
@@ -0,0 +1,104 @@
+//GitHub does not allow reading of external text-files.
+//Random level array
+const gamePlan = {
+  start : [
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  " @x             ",
+  "gggggg     ggggg",
+  "dddddd     ddddd",
+  "ddddddwwwwwddddd"
+  ],
+  end : [
+  "                ",
+  "                ",
+  "               x",
+  "               x",
+  "             V x",
+  "            gggg",
+  "            dddd",
+  "       =  g dddd",
+  "gggggg gggd|dddd",
+  "dddddd|ddddwdddd",
+  "ddddddwddddddddd"
+  ],
+  plains : [
+  [
+  "                ",
+  "                ",
+  "             o  ",
+  "                ",
+  "     d    d     ",
+  "                ",
+  " d              ",
+  "                ",
+  "gggggg     ggggg",
+  "dddddd | | ddddd",
+  "ddddddwwwwwddddd"
+  ], [
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "           o    ",
+  "                ",
+  "     ggg        ",
+  "                ",
+  "ggg        ggggg",
+  "ddd |      ddddd",
+  "dddwwwwwwwwddddd"
+  ], [
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "     o    o     ",
+  "                ", 
+  "                ",
+  "ggg gggg gggg gg",
+  "ddd|dddd|dddd|dd",
+  "dddwddddwddddwdd"
+  ], [
+  "                ",
+  "                ",
+  "                ",
+  "        o       ",
+  "                ",
+  "                ",
+  "    dddddddd    ",
+  "                ",
+  "g              g",
+  "d |          | d",
+  "dwwwwwwwwwwwwwwd"
+  ], [
+  "                ",
+  "                ",
+  "                ",
+  "             o  ",
+  "                ",
+  "      d   d     ",
+  "  d             ",
+  "                ",
+  "ggggg ggggggg gg",
+  "ddddd|ddddddd|dd",
+  "dddddwdddddddwdd"
+  ], [
+  "                ",
+  "                ",
+  "                ",
+  "                ",
+  "              o ",
+  "                ",
+  "           d    ",
+  "       d        ",
+  "gggg           g",
+  "dddd     |   | d",
+  "ddddwwwwwwwwwwwd"
+  ]]
+};
